73204e143dfdb53b15816ac364ec37e9a95ed18e
Riyathakur235 <riyathakur8052@gmail.com>
2026-04-16 23:43:29 +0530

    cart

--- my-react-app/src/AddToCart.jsx
+++ my-react-app/src/AddToCart.jsx
@@ -1,44 +1,13 @@
-import React from 'react'
-
-const AddToCart = ({cart}) => {
-  return (
-    <div>
-        {
-            cart.map((a)=>{
-                return (<>
-                <div id='card'> 
-                    <img  src={a.image}/>
-            <p>{a.name}</p>
-            <p> Rating: {a.rating}</p>
-         <button  onClick={()=> SetCart([...cart,a])}>add </button>
-         </div>
-                </>)
-
-            })
-        }
-    </div>
-  )
-}
-
-export default AddToCart
-
-
-
-
-// import React, { useContext } from 'react'
-// import Context from './Context'
+// import React from 'react'
 
 // const AddToCart = ({cart}) => {
-//   let {state ,dispatch} =    useContext(Context)
-//   console.log(state.cart,"hehehe");
-  
 //   return (
-//     <div>  
+//     <div>
 //         {
-//             state.cart.map((a)=>{
+//             cart.map((a)=>{
 //                 return (<>
 //                 <div id='card'> 
-                   
+//                     <img  src={a.image}/>
 //             <p>{a.name}</p>
 //             <p> Rating: {a.rating}</p>
 //          <button  onClick={()=> SetCart([...cart,a])}>add </button>
@@ -47,8 +16,39 @@ export default AddToCart
 
 //             })
 //         }
-//         </div>
+//     </div>
 //   )
 // }
 
 // export default AddToCart
+
+
+
+
+import React, { useContext } from 'react'
+import Context from './Context'
+
+const AddToCart = ({cart}) => {
+  let {state ,dispatch} =    useContext(Context)
+  console.log(state.cart,"hehehe");
+  
+  return (
+    <div>  
+        {
+            state.cart.map((a)=>{
+                return (<>
+                <div id='card'> 
+                   
+            <p>{a.name}</p>
+            <p> Rating: {a.rating}</p>
+         <button  onClick={()=> SetCart([...cart,a])}>add </button>
+         </div>
+                </>)
+
+            })
+        }
+        </div>
+  )
+}
+
+export default AddToCart
